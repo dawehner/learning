23257176b69fbf841024d88d3ddb2e01d4b659b3
Daniel Wehner <dawehner@googlemail.com>
2019-06-20 20:22:34 +0100

    playing around with processing

--- processing/demo1/sketch.js
+++ processing/demo1/sketch.js
@@ -1,4 +1,5 @@
 var system;
+var startTime = Date.now();
 
 function setup() {
   createCanvas(720, 400);
@@ -7,74 +8,83 @@ function setup() {
 
 function draw() {
   background(51);
-  system.addParticle();
+
+  const time = Date.now() - startTime;
+
+  system.addParticle(
+    createVector(
+      width/2 + 100 * Math.cos(2 * time),
+      height/2 + 100 * Math.sin(3 * time)
+    )
+  );
   system.run();
 }
 
 // A simple Particle class
-var Particle = function(position) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
-  this.position = position.copy();
-  this.lifespan = 255.0;
+class Particle {
+  constructor(position) {
+    this.acceleration = createVector(0, 0.05);
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.position = position.copy();
+    this.lifespan = 255.0;
 
-  const colors = [
-    [255,255,178],
-    [254,217,118],
-    [254,178,76],
-    [253,141,60],
-    [240,59,32],
-    [189,0,38],
-  ];
-  const index = Math.floor(Math.random() * colors.length);
-  console.log(index);
-  this.color = colors[index];
-};
-
-Particle.prototype.run = function() {
-  this.update();
-  this.display();
-};
-
-// Method to update position
-Particle.prototype.update = function(){
-  this.velocity.add(this.acceleration);
-  this.position.add(this.velocity);
-  this.lifespan -= 2;
-};
-
-// Method to display
-Particle.prototype.display = function() {
-  stroke(200, this.lifespan);
-  strokeWeight(2);
-  fill(this.color[0], this.color[1], this.color[2]);
-  ellipse(this.position.x, this.position.y, 12, 12);
-};
-
-// Is the particle still useful?
-Particle.prototype.isDead = function(){
-  if (this.lifespan < 0) {
-    return true;
-  } else {
-    return false;
+    const colors = [
+      [255,255,178],
+      [254,217,118],
+      [254,178,76],
+      [253,141,60],
+      [240,59,32],
+      [189,0,38],
+    ];
+    const index = Math.floor(Math.random() * colors.length);
+    this.color = colors[index];
   }
-};
 
-var ParticleSystem = function(position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
+  run() {
+    this.update();
+    this.display();
+  }
 
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+  }
 
-ParticleSystem.prototype.run = function() {
-  for (var i = this.particles.length-1; i >= 0; i--) {
-    var p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
+  display() {
+    stroke(200, this.lifespan);
+    strokeWeight(2);
+    fill(this.color[0], this.color[1], this.color[2]);
+    ellipse(this.position.x, this.position.y, 12, 12);
+  }
+
+  isDead() {
+    if (this.lifespan < 0) {
+      return true;
+    } else {
+      return false;
     }
   }
-};
+}
+
+class ParticleSystem {
+  constructor(position) {
+    this.origin = position.copy();
+    this.particles = [];
+  }
+
+  addParticle(origin) {
+    this.particles.push(new Particle(origin || this.origin));
+  }
+
+  run() {
+    for (var i = this.particles.length-1; i >= 0; i--) {
+      var p = this.particles[i];
+      p.run();
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  }
+
+}
