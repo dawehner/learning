bc8c7f66f04d4d6948244b6500edcf37e673aee2
Daniel Wehner <dawehner@googlemail.com>
2019-09-06 20:09:06 +0100

    initial commit

---
+++ laravel/mindmap/database/seeds/DocumentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Document;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DocumentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('mindmap_document')
+            ->delete();
+        
+        Document::create([
+            'title' => 'My initial mindmap',
+        ]);
+        Document::create([
+            'title' => 'My other mindmap',
+        ]);
+    }
+}
