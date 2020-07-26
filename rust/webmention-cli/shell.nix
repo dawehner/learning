804e735bfe441f129f6d176f036e373044270e66
Daniel Wehner <dawehner@googlemail.com>
2020-07-26 09:57:26 +0100

    test

--- rust/webmention-cli/shell.nix
+++ rust/webmention-cli/shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-    buildInputs = [ pkgs.rustup ]
+    buildInputs = [ pkgs.rustup pkgs.openssl ]
       ++ pkgs.stdenv.lib.optionals pkgs.stdenv.isDarwin [pkgs.darwin.apple_sdk.frameworks.Security];
 }
