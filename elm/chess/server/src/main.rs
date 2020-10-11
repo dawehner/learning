bec5216681f9e66190ca2df0cc300693f641c19a
Daniel Wehner <dawehner@googlemail.com>
2020-10-11 08:01:22 +0100

    Start server

---
+++ elm/chess/server/src/main.rs
@@ -0,0 +1,49 @@
+use std::thread;
+use websocket::sync::Server;
+use websocket::OwnedMessage;
+
+fn main() {
+    let server = Server::bind("127.0.0.1:2794").unwrap();
+
+    for request in server.filter_map(Result::ok) {
+        // New thread for each connction for now.
+        thread::spawn(|| {
+            println!("{:?}", request.protocols());
+            if !request.protocols().contains(&"chess-websocket".to_string()) {
+                request.reject().unwrap();
+                return;
+            }
+
+            // Accepts the connection itself.
+            let mut client = request.use_protocol("chess-websocket").accept().unwrap();
+            let ip = client.peer_addr().unwrap();
+
+            println!("Connection from {}", ip);
+
+            let message = OwnedMessage::Text("Hello".to_string());
+            client.send_message(&message).unwrap();
+
+            let (mut receiver, mut sender) = client.split().unwrap();
+
+            for message in receiver.incoming_messages() {
+                let message = message.unwrap();
+
+                match message {
+                    OwnedMessage::Close(_) => {
+                        let message = OwnedMessage::Close(None);
+                        sender.send_message(&message).unwrap();
+                        println!("Client {} disconnected", ip);
+                        return;
+                    }
+
+                    OwnedMessage::Ping(ping) => {
+                        let message = OwnedMessage::Pong(ping);
+                        sender.send_message(&message).unwrap();
+                    }
+
+                    _ => sender.send_message(&message).unwrap(),
+                }
+            }
+        });
+    }
+}
