ea07328dda57cfb8517cde6c3c2a48afc0e708d7
Daniel Wehner <dawehner@googlemail.com>
2020-12-29 20:09:43 +0000

    fix run script

--- second-brain/recipes/rerun.sh
+++ second-brain/recipes/rerun.sh
@@ -4,4 +4,4 @@ set -e
 
 rm -Rf download
 
-nix-shell -p python38Packages.pyyaml python38Packages.jinja2 -c './get-convert-paprika.py'
+nix-shell -p python38Packages.pyyaml python38Packages.jinja2 --command './get-convert-paprika.py'
